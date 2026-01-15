127dd27933c1d289287be60ff7a3ca54d2aa00da
Srishti3106 <srishtimadaan211@gmail.com>
2026-01-15 14:28:34 +0000

    ....

---
+++ code3/code3_1.c
@@ -0,0 +1,17 @@
+//Basic/Common Skeleton used for writing any C++ program.
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //int
+    int x = 10;//This is how you assign something into an integer Data type.
+    return 0;
+}
