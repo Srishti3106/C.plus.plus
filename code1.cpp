2cc4fb56a9fc4690cb1038fea213ed0a6e548086
Srishti3106 <srishtimadaan211@gmail.com>
2026-01-15 10:17:11 +0000

    The Start.

---
+++ code1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main() {
+    std:: cout << "Hey Srishti!";//Basic code to print something
+    return 0;
+}
---
+++ code1_1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main() {
+    std :: cout << "Hey Srishti!";//they will be printed in the same line
+    std :: cout << "Hey Srishti!";
+    return 0;
+}
